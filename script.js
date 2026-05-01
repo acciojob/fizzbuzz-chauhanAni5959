9c0f303c929721c460fc9b99d4b71626b1f5e539
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2026-05-01 12:00:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function PrintNumber() {
+	for(let i = 0; i <= 20; i++){
+		if (i % 3 === 0) {
+			alert('Fizz');
+			
+		}
+		else if (i % 5 === 0) {
+			alert('Buzz');
+			
+		}
+		else if(i % 3 === 0 && i % 5 === 0 ){
+			alert('FizzBuzz');
+			
+		}
+		
+		
+	}
+	
+}
